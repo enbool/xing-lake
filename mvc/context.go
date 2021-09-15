e211c3de3e6905aa206e4cdb4e77354be02b8936
enbool <enbool@163.com>
2021-09-15 11:07:11 +0800

    add context

---
+++ mvc/context.go
@@ -0,0 +1,77 @@
+package mvc
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+/**
+ * @Description:
+ *
+ *
+ * @Author: wumin2
+ * @Date:  2021/9/14 19:06
+ */
+// Http request Context
+type Context struct {
+	Writer   http.ResponseWriter
+	Request http.Request
+
+	Path       string
+	Method     string
+	StatusCode int
+}
+
+func newConext(writer http.ResponseWriter, request http.Request) *Context {
+	return &Context{
+		Writer:   writer,
+		Request: request,
+		Path:    request.URL.Path,
+		Method:  request.Method,
+	}
+}
+
+// PostForm is func
+func (c *Context) PostForm(key string) string {
+	return c.Request.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Request.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}){
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) Json(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
