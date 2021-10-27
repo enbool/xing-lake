c5ae388a8de5365c2caaa1c155e199aaf8908400
enbool <enbool@163.com>
2021-10-27 19:11:33 +0800

    Update lru.go

--- lake/lru.go
+++ lake/lru.go
@@ -1,4 +1,4 @@
-package lake
+package lru
 
 import "container/list"
 
@@ -12,7 +12,53 @@ import "container/list"
 
 type Cache struct {
 	maxBytes int64
-	nbytes int64
-	ll *list.List
-	cache map[string] *list.Element
+	nbytes   int64
+	ll       *list.List
+	cache    map[string]*list.Element
+
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, onEvicted func(string2 string, value Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if element, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(element)
+		kv := element.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+func (c *Cache) RemoveOldest() {
+	element := c.ll.Back()
+	if element != nil {
+		c.ll.Remove(element)
+		kv := element.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil{
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add()  {
+	
 }
