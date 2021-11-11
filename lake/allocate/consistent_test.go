61f5a64c5ac5abcf023c1acb1780a668d52f0ff5
enbool <enbool@163.com>
2021-11-12 00:06:57 +0800

    add consistent hash test

---
+++ lake/allocate/consistent_test.go
@@ -0,0 +1,32 @@
+package allocate
+
+import (
+	"strconv"
+	"testing"
+)
+
+/**
+ * @Description:
+ *
+ *
+ * @Author: wumin2
+ * @Date:  2021/11/11 23:56
+ */
+
+func TestHash(t *testing.T) {
+	hash := New(100, func(key []byte) uint32 {
+		i, _ := strconv.Atoi(string(key))
+		return uint32(i)
+	})
+
+	// Given the above hash function, this will give replicas with "hashes":
+	// 2, 4, 6, 12, 14, 16, 22, 24, 26
+	hash.Add("6", "4", "2")
+
+	testCases := map[string]string{
+		"2":  "2",
+		"11": "2",
+		"23": "4",
+		"27": "2",
+	}
+}
